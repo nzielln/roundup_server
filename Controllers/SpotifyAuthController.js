945ced766ef88976f30d3d86b1586b04566be17e
Ellah Nzikoba <ellahnzikoba@gmail.com>
2022-12-21 14:18:43 -0500

    first commit

---
+++ Controllers/SpotifyAuthController.js
@@ -0,0 +1,130 @@
+import { CLIENT_ID, STATE, CLIENT_SECRET, REDIRECT_URI } from "../server.js";
+import querystring from 'querystring'
+import axios from "axios";
+
+const getCredentials = (req, res) => {
+    console.info("ACCESS REQUESTED!")
+
+    const state = generateRandomString(16)
+    res.cookie(STATE, state)
+
+    const scope = [
+        "user-read-private",
+        "user-read-email",
+        "streaming",
+        "user-read-playback-state",
+        "user-modify-playback-state",
+        "user-read-currently-playing",
+        "user-read-playback-position",
+        "user-top-read"].join(' ')
+    
+    const params = querystring.stringify({
+        response_type: 'code',
+        client_id: CLIENT_ID,
+        scope: scope,
+        redirect_uri: REDIRECT_URI,
+        state: state
+    })
+
+    res.redirect('https://accounts.spotify.com/authorize?' + params)
+
+}
+
+const callback = (req, res) => {
+    console.info("CALLBACK CALLED!")
+
+    const code = req.query.code || null;
+
+    const authOptions = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            code: code,
+            redirect_uri: REDIRECT_URI,
+            grant_type: 'authorization_code'
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+        }
+    }
+
+    axios(authOptions).then(response => {
+        console.log("CALLBACK RESPONSE: ")
+        console.log(response.data)
+        const { access_token, token_type, refresh_token, expires_in } = response.data
+
+        const options = {
+            method: 'get',
+            url: 'https://api.spotify.com/v1/me',
+            headers: { 'Authorization': `${token_type} ${access_token}` }
+        }
+
+        axios(options).then(response => {
+            console.log("API.SPOTIFY.COM/V1/ME RESPONSE: ")
+            console.log(response.data)
+
+            const params = querystring.stringify({
+                access_token, refresh_token, expires_in
+            })
+
+            res.redirect(`${process.env.ORIGIN}/home/?${params}`)
+        }).catch(error => {
+            console.log(error)
+        })
+
+    }).catch(error => {
+        console.log(error)
+        res.redirect(process.env.ORIGIN + '/welcome')
+    })
+
+}
+
+const refresh = (req, res) => {
+    // requesting access token from refresh token
+    console.info("REFRESH REQUESTED!")
+
+    const refresh_token = req.params.refreshToken;
+    console.log(refresh_token)
+
+    const authOptions = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + (Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+        },
+        data: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        }
+    };
+
+
+    axios(authOptions).then(response => {
+        console.log("RESPONSE RECIEVED")
+        console.info(response.data)
+        res.send(response.data);
+    }).catch(error => {
+        console.error(error)
+        res.send(error)
+    })
+}
+
+const generateRandomString = (length) => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+const SpotifyAuthController = (app) => {
+    app.get("/wp/api/login", getCredentials)
+    app.get("/wp/api/callback", callback)
+    app.get("/wp/api/refresh/:refreshToken", refresh)
+}
+
+
+export default SpotifyAuthController
---
+++ server.js
@@ -0,0 +1,35 @@
+import multer from "multer"
+import cors from "cors";
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express, { application } from "express"
+import SpotifyAuthController from "./Controllers/SpotifyAuthController.js";
+import cookieParser from "cookie-parser";
+
+export const CLIENT_ID = process.env.CLIENT_ID
+export const CLIENT_SECRET = process.env.CLIENT_SECRET
+export const REDIRECT_URI = process.env.REDIRECT_URI
+export const STATE = "spotify_auth_state"
+
+const app = express()
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images')
+    },
+    filename: function (req, file, cb) {
+     cb(null, file.originalname)
+ }   
+})
+
+app.use(cors({
+    credentials: true,
+    origin: [process.env.ORIGIN],
+    methods: ["GET","PUT","POST","DELETE"],
+  }))
+  .use(cookieParser());
+
+
+SpotifyAuthController(app)
+
+app.use(express.static('Images'))
+app.listen(4000)
